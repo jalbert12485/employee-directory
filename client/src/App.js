53e9bf2911977b956cd3666ae1e49945a9b4a499
Justin Albert <jalbert@carthage.edu>
2020-11-18 17:48:52 -0600

    working app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import EmployeeList from "./components/EmployeeList";
+import employees from "./employees.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const styles = {
+  container: {
+    borderStyle: "solid",
+    borderWidth: "20px",
+    borderColor: "#A37871",
+    backgroundColor: "#F3D8C7"
+  },
+  li: {
+    borderStyle: "solid",
+    borderWidth: "1px",
+    borderColor: "black"
+  }
+};
+
+class App extends Component {
+  state = {
+    "employees": employees,
+    "filteredEmployees": employees
+  };
+
+  handleLocationChange= location=>{
+    let array=[];
+    if(location !== "all"){
+      array=this.state.employees.filter(employee => employee.Region === location);
+    }else{
+      array=this.state.employees;
+    }
+    this.setState({ filteredEmployees: array });
+  }
+
+  handleSortChange= sortOption => {
+    let array=[];
+    array=this.state.filteredEmployees.sort((a, b) => {
+      if (a[sortOption] < b[sortOption]) {
+        return -1;
+      }
+      if (a[sortOption] > b[sortOption]) {
+        return 1;
+      }
+    
+      return 0;
+    });
+
+    this.setState({filteredEmployees: array});
+  }
+
+  render() {
+    return (
+      <div className="container p-4 mr-auto ml-auto" style={styles.container}>
+        <EmployeeList employees={this.state.filteredEmployees} locationChange={this.handleLocationChange} sortChange={this.handleSortChange}/>
+      </div>
+    );
+  }
 }
 
 export default App;
